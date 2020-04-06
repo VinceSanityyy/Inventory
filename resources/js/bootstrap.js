4dcd84c577ed90822f9047e3f71f11b2bffa573d
vincesanityyy <vbustillo97@gmail.com>
2020-04-06 22:17:15 +0800

    initial commit, added template and dependencies

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,29 @@
+window._ = require('lodash');
+
+
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+require('admin-lte');
+require('admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js');
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+
