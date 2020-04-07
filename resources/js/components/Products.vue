e9ae7f0a8adc2d0e1c23860f705339cda4aa3443
vincesanityyy <vbustillo97@gmail.com>
2020-04-07 21:35:43 +0800

    added master layout and components

---
+++ resources/js/components/Products.vue
@@ -0,0 +1,39 @@
+<template>
+ <section class="content">
+     <div class="row">
+        <div class="col-md-12">
+             <div class="card">
+            <div class="card-header">
+                 <h3 class="card-title">Products</h3>
+            </div>
+            <div class="card-body" >
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Test</th>
+                            <th>Test</th>
+                            <th>Test</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+         </div>
+        </div>
+     </div>
+ </section>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
